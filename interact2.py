8f2de53aa5ee792a0b94f72abdc88a0aa1ceb56c
SongDohwan <49189233+SongDohwan@users.noreply.github.com>
2019-10-15 12:43:27 +0900

    Add files via upload

--- interact2.py
+++ interact2.py
@@ -8,6 +8,8 @@ from argparse import ArgumentParser
 from itertools import chain
 from pprint import pformat
 import warnings
+import sys
+import numpy as np
 
 import torch
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
@@ -16,11 +18,15 @@ import torch.nn.functional as F
 from pytorch_transformers import OpenAIGPTLMHeadModel, OpenAIGPTTokenizer, GPT2LMHeadModel, GPT2Tokenizer
 from train import SPECIAL_TOKENS, build_input_from_segments, add_special_tokens_
 from utils import get_dataset_personalities, download_pretrained_model
+from sklearn.metrics.pairwise import cosine_similarity
 
+import os
+import time
+import matplotlib
 import matplotlib.pyplot as plt
 
 
-def top_k_top_p_filtering(logits, tokenizer, history, args, B_tokenizer, top_k=0, top_p=0.0, filter_value=-float('Inf'),
+def top_k_top_p_filtering(logits, tokenizer, history, args, B_tokenizer, B_model, top_k=0, top_p=0.0, filter_value=-float('Inf'),
                           current_output=None):
     """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
         Args:
@@ -59,8 +65,47 @@ def top_k_top_p_filtering(logits, tokenizer, history, args, B_tokenizer, top_k=0
         prev = torch.topk(probs, 1)[1] if args.no_sample else torch.multinomial(probs, len(index))
         text = []
         last_utt = history[-1]
+        last = tokenizer.decode(last_utt, skip_special_tokens=True)
 
-        print(len(index))
+        marked_text2 = "[CLS] " + last + " [SEP]"
+        tokenized_text2 = B_tokenizer.tokenize(marked_text2)
+        indexed_tokens2 = B_tokenizer.convert_tokens_to_ids(tokenized_text2)
+        segments_ids2 = [1] * len(tokenized_text2)
+        tokens_tensor2 = torch.tensor([indexed_tokens2])
+        segments_tensors2 = torch.tensor([segments_ids2])
+        with torch.no_grad():
+            encoded_layers2, _ = B_model(tokens_tensor2, segments_tensors2)
+        sentence_embedding2 = torch.mean(encoded_layers2[11], 1)
+        #print(sentence_embedding2[0])
+
+        token_embeddings2 = []
+        batch_i = 0
+
+        # For each token in the sentence...
+        for token_i in range(len(tokenized_text2)):
+
+            # Holds 12 layers of hidden states for each token
+            hidden_layers = []
+
+            # For each of the 12 layers...
+            for layer_i in range(len(encoded_layers2)):
+                # Lookup the vector for `token_i` in `layer_i`
+                vec2 = encoded_layers2[layer_i][batch_i][token_i]
+
+                hidden_layers.append(vec2)
+
+            token_embeddings2.append(hidden_layers)
+
+        #concatenated_last_4_layers = [torch.cat((layer[-1], layer[-2], layer[-3], layer[-4]), 0) for layer in
+        #                              token_embeddings]  # [number_of_tokens, 3072]
+
+        summed_last_4_layers2 = [torch.sum(torch.stack(layer)[-4:], 0) for layer in
+                                token_embeddings2]  # [number_of_tokens, 768]
+
+        #print(len(index))
+        print("-------------------------------------------------------------")
+
+
         for i in prev:
             text.append(i.item())
         for i in text:
@@ -69,24 +114,62 @@ def top_k_top_p_filtering(logits, tokenizer, history, args, B_tokenizer, top_k=0
             raw_text=tokenizer.decode(x, skip_special_tokens=True)
             #raw_text = tokenizer.decode(x)
             marked_text1 = "[CLS] " + raw_text + " [SEP]"
-            print(marked_text1)
-            marked_text2 = "[CLS] " + last_utt + " [SEP]"
-            print(marked_text2)
             tokenized_text1 = B_tokenizer.tokenize(marked_text1)
-            print (tokenized_text1)
-            tokenized_text2 = B_tokenizer.tokenize(marked_text2)
-            print (tokenized_text2)
             indexed_tokens1 = B_tokenizer.convert_tokens_to_ids(tokenized_text1)
-            indexed_tokens2 = B_tokenizer.convert_tokens_to_ids(tokenized_text2)
-            print(indexed_tokens1)
-            print(indexed_tokens2)
-            x.clear()
+            segments_ids1 = [0] * len(tokenized_text1)
+            tokens_tensor1 = torch.tensor([indexed_tokens1])
+            segments_tensors1 = torch.tensor([segments_ids1])
+            #outputs = model(tokens_tensor1)
+            #last_hidden_states = outputs[0]
+            with torch.no_grad():
+                encoded_layers1, _ = B_model(tokens_tensor1, segments_tensors1)
+            sentence_embedding1 = torch.mean(encoded_layers1[11], 1)
+            #print(sentence_embedding1[0])
+            sim = cosine_similarity(sentence_embedding1.reshape(1,-1), sentence_embedding2.reshape(1,-1))
+            print(raw_text)
+            print("sen_sim: ")
+            print(sim)
+            token_embeddings1 = []
 
+            # For each token in the sentence...
+            for token_i in range(len(tokenized_text1)):
+
+                # Holds 12 layers of hidden states for each token
+                hidden_layers = []
+
+                # For each of the 12 layers...
+                for layer_i in range(len(encoded_layers1)):
+                    # Lookup the vector for `token_i` in `layer_i`
+                    vec1 = encoded_layers1[layer_i][batch_i][token_i]
+
+                    hidden_layers.append(vec1)
+
+                token_embeddings1.append(hidden_layers)
+
+            #concatenated_last_4_layers = [torch.cat((layer[-1], layer[-2], layer[-3], layer[-4]), 0) for layer in
+             #                             token_embeddings]  # [number_of_tokens, 3072]
+
+            summed_last_4_layers1 = [torch.sum(torch.stack(layer)[-4:], 0) for layer in
+                                     token_embeddings1]  # [number_of_tokens, 768]
+            temp = []
+            for i in summed_last_4_layers1:
+                a = []
+                for j in summed_last_4_layers2:
+                    #print("cos: ")
+                    #print(cosine_similarity(i.reshape(1,-1), j.reshape(1,-1))[0][0])
+                    a.append(cosine_similarity(i.reshape(1,-1), j.reshape(1,-1))[0][0])
+                temp.append(max(a))
+            #print("temp: ")
+            #print(temp)
+            print("sim: ")
+            sim = sum(temp) / len(temp)
+            print(sim)
+            x.clear()
 
     return logits
 
 
-def sample_sequence(personality, history, tokenizer, B_tokenizer, model, args, current_output=None):
+def sample_sequence(personality, history, tokenizer, B_tokenizer, model, args, B_model, current_output=None):
     special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
     if current_output is None:
         current_output = []
@@ -103,7 +186,7 @@ def sample_sequence(personality, history, tokenizer, B_tokenizer, model, args, c
         if isinstance(logits, tuple):  # for gpt2 and maybe others
             logits = logits[0]
         logits = logits[0, -1, :] / args.temperature
-        logits = top_k_top_p_filtering(logits, tokenizer, history, args, B_tokenizer, top_k=top_k, top_p=top_p,
+        logits = top_k_top_p_filtering(logits, tokenizer, history, args, B_tokenizer, B_model, top_k=top_k, top_p=top_p,
                                        current_output=current_output)
         probs = F.softmax(logits, dim=-1)
 
@@ -162,6 +245,8 @@ def run():
     model.to(args.device)
     add_special_tokens_(model, tokenizer)
     B_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    B_model = BertModel.from_pretrained('bert-base-uncased')
+    B_model.eval()
 
     logger.info("Sample a personality")
     personalities = get_dataset_personalities(tokenizer, args.dataset_path, args.dataset_cache)
@@ -169,6 +254,7 @@ def run():
     logger.info("Selected personality: %s", tokenizer.decode(chain(*personality)))
 
     history = []
+
     while True:
         raw_text = input(">>> ")
         while not raw_text:
@@ -176,12 +262,98 @@ def run():
             raw_text = input(">>> ")
         history.append(tokenizer.encode(raw_text))
         with torch.no_grad():
-            out_ids = sample_sequence(personality, history, tokenizer, B_tokenizer, model, args)
+            out_ids = sample_sequence(personality, history, tokenizer, B_tokenizer, model, args, B_model)
         history.append(out_ids)
         history = history[-(2 * args.max_history + 1):]
         out_text = tokenizer.decode(out_ids, skip_special_tokens=True)
-
         print(out_text)
+'''
+####################################
+        marked_text2 = "[CLS] " + "hi. how are you?" + " [SEP]"
+        tokenized_text2 = B_tokenizer.tokenize(marked_text2)
+        indexed_tokens2 = B_tokenizer.convert_tokens_to_ids(tokenized_text2)
+        segments_ids2 = [1] * len(tokenized_text2)
+        tokens_tensor2 = torch.tensor([indexed_tokens2])
+        segments_tensors2 = torch.tensor([segments_ids2])
+        with torch.no_grad():
+            encoded_layers2, _ = B_model(tokens_tensor2, segments_tensors2)
+        sentence_embedding2 = torch.mean(encoded_layers2[11], 1)
+        # print(sentence_embedding2[0])
+
+        token_embeddings2 = []
+        batch_i = 0
+
+        # For each token in the sentence...
+        for token_i in range(len(tokenized_text2)):
+
+            # Holds 12 layers of hidden states for each token
+            hidden_layers = []
+
+            # For each of the 12 layers...
+            for layer_i in range(len(encoded_layers2)):
+                # Lookup the vector for `token_i` in `layer_i`
+                vec2 = encoded_layers2[layer_i][batch_i][token_i]
+
+                hidden_layers.append(vec2)
+
+            token_embeddings2.append(hidden_layers)
+
+        # concatenated_last_4_layers = [torch.cat((layer[-1], layer[-2], layer[-3], layer[-4]), 0) for layer in
+        #                              token_embeddings]  # [number_of_tokens, 3072]
+
+        summed_last_4_layers2 = [torch.sum(torch.stack(layer)[-4:], 0) for layer in
+                                 token_embeddings2]  # [number_of_tokens, 768]
+
+        marked_text1 = "[CLS] " + "i'm good. i am working on my airplane." + " [SEP]"
+        tokenized_text1 = B_tokenizer.tokenize(marked_text1)
+        indexed_tokens1 = B_tokenizer.convert_tokens_to_ids(tokenized_text1)
+        segments_ids1 = [0] * len(tokenized_text1)
+        tokens_tensor1 = torch.tensor([indexed_tokens1])
+        segments_tensors1 = torch.tensor([segments_ids1])
+        with torch.no_grad():
+            encoded_layers1, _ = B_model(tokens_tensor1, segments_tensors1)
+        sentence_embedding1 = torch.mean(encoded_layers1[11], 1)
+        # print(sentence_embedding2[0])
+
+        token_embeddings1 = []
+        batch_i = 0
+
+        # For each token in the sentence...
+        for token_i in range(len(tokenized_text1)):
+
+            # Holds 12 layers of hidden states for each token
+            hidden_layers = []
+
+            # For each of the 12 layers...
+            for layer_i in range(len(encoded_layers1)):
+                # Lookup the vector for `token_i` in `layer_i`
+                vec1 = encoded_layers1[layer_i][batch_i][token_i]
+
+                hidden_layers.append(vec1)
+
+            token_embeddings1.append(hidden_layers)
+
+        # concatenated_last_4_layers = [torch.cat((layer[-1], layer[-2], layer[-3], layer[-4]), 0) for layer in
+        #                              token_embeddings]  # [number_of_tokens, 3072]
+
+        summed_last_4_layers1 = [torch.sum(torch.stack(layer)[-4:], 0) for layer in
+                                 token_embeddings1]  # [number_of_tokens, 768]
+        temp = []
+        for i in summed_last_4_layers1:
+            a = []
+            for j in summed_last_4_layers2:
+                print("cos: ")
+                print(cosine_similarity(i.reshape(1,-1), j.reshape(1,-1))[0][0])
+                a.append(cosine_similarity(i.reshape(1, -1), j.reshape(1, -1))[0][0])
+            temp.append(max(a))
+        print("temp: ")
+        print(temp)
+        print("sim: ")
+        sim = sum(temp) / len(temp)
+        print(sim)
+###############################################################################
+        '''
+
 
 
 if __name__ == "__main__":
